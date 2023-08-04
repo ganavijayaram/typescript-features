8009a03f7220299f28d59aeba6077373fe4f3dbe
ganavijayaram <jayaram.g@northeastern.edu>
2023-08-04 17:54:40 -0400

    annotations for variables

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -3,6 +3,7 @@
 //telling  TS the type of value to expect
 //primitive
 let numApples: number = 5
+//numApples = "Hello" ->throws error
 let speed: string = 'fast'
 let nothingmuch: null = null
 let nothing: undefined = undefined
@@ -30,9 +31,41 @@ let point: {x: number; y:number}= {
 
 //Function
 //(i: number) => void -> tells what arguments and what is the return type
+// the above is annotations
 const logNumber: (i: number) => void = (i: number) => {
   console.log(`Value of i = ${i}`)
 }
 
-//numApples = "Hello" ->throws error
+logNumber(10)
 
+
+//when to use annotations
+//1. Functions returning any
+const obj = '{"x":10, "y":20}'
+//JSON.parse is a function which return any-> which means it can return any type thats why coordinates is any
+const coordinates = JSON.parse(obj)
+//coordinates.asbjasbdjsb -> will not throw error because it is of type any
+
+//fix 1
+const coordinates1: {x:number; y:number} = JSON.parse(obj)
+//coordinates.asbjasbdjsb -> will throw error because it has a type
+
+//2. when declaration and initialisation are on different lines
+let words = ['dog', 'beige']
+//Here
+let found: boolean
+
+for (let i = 0; i < words.length; i++) {
+  if(words[i] === 'Ganavi') {
+    found = true
+  }
+}
+
+//variable whose type cannot be inferred
+let numbers = [1, 2, -2]
+let numFound: boolean | number = false
+for (let i = 0; i < numbers.length; i++) {
+  if(numbers[i] > 0) {
+    numFound = numbers[i]
+  }
+}
