196dc1fbd96547c3da45cdbd79147a6c72e02498
ganavijayaram <jayaram.g@northeastern.edu>
2023-08-04 11:56:28 -0400

    Working with types

---
+++ types.ts
@@ -0,0 +1,16 @@
+//For this type, we can know all the properties and methods it is associated with
+const date = new Date()
+
+const person = {
+  age: 20
+}
+//const people as person -  throws error
+
+//Putting inside the class is better than the above example, because when you hover
+//for person it shows too many details
+//for red, it shows Color
+class Color {
+
+}
+
+const red = new Color()
